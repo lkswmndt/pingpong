70018e3d3f38dff578f58aa427fbf774ebedc065
lkswmndt <hibb280709@gmail.com>
2025-09-24 19:57:13 +0700

    add tennis ball

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ window = display.set_mode((win_width, win_height))
 background = (85, 255, 255)
 window.fill(background)
 
+font.init()
+font_game = font.SysFont('Arial', 36)
+font2 = font.SysFont('Arial', 72)
+right_win = font2.render('PLAYER 1 WIN', True, (255,255,0))
+left_win = font2.render('PLAYER 2 WIN', True, (255,0,0))
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image,player_x,player_y,size_x,size_y,player_speed):
@@ -36,19 +41,43 @@ class Player(GameSprite):
         if keys[K_s] and self.rect.y < win_height-80:
             self.rect.y += self.speed
 
-left_platform = Player('left_platform.png', 10, win_height/2,20,80,20)
-right_platform = Player('right_platform.png', win_width-30, win_height/2,20,80,20)
+left_platform = Player('left_platform.png', 10, win_height/2,20,100,20)
+right_platform = Player('right_platform.png', win_width-30, win_height/2,20,100,20)
+tennis = GameSprite('tennis_ball.png',win_width/2,win_height/2,30,30,20)
 
 finish = False
 run = True
+tennis_speedx = 10
+tennis_speedy = 8
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
 
     if not finish:
+        window.fill(background)
         left_platform.update_left()
         right_platform.update_right()
+
+        tennis.rect.x += tennis_speedx
+        tennis.rect.y += tennis_speedy
+
+        if sprite.collide_rect(left_platform, tennis) or sprite.collide_rect(right_platform, tennis):
+            tennis_speedx *= -1
+            tennis_speedy *= -1
+        
+        if tennis.rect.y > win_height-30 or tennis.rect.y < 0:
+            tennis_speedy *= -1
+
+        if tennis.rect.x < 0:
+            finish = True
+            window.blit(right_win, (100,win_height/2))
+
+        if tennis.rect.x > win_width:
+            finish = True
+            window.blit(left_win, (100,win_height/2))
+
+        tennis.reset()
         left_platform.reset()
         right_platform.reset()
 
